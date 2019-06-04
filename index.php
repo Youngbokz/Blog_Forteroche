16b3886542a8995204e267f5366757a48af969bd
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-04 22:35:42 +0200

    Add try{} & catch(Exception ) for error message

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
 require('controller/frontend.php');
-
-if(isset($_GET['action'])){ // We check if there's action in URL. Both case we send to home page
-    if($_GET['action '] == "listPosts")
+try
+{
+    if(isset($_GET['action'])){ // We check if there's action in URL. Both case we send to home page
+        if($_GET['action '] == "listPosts")
+        {
+            listPosts();
+        }
+    }
+    else // Even in this case display home page 
     {
         listPosts();
     }
-}
-else // Even in this case display home page 
-{
-    listPosts();
-}
 
-?>
+}
+catch(Exception $e)
+{
+    $errorMessage = $e->getMessage();
+    require('view/errorView.php');
+}
