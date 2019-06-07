d9127b3a751e7886864b5f1136c9374c6f648097
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-07 08:50:03 +0200

    Add local bootstrap

--- views/frontend/template.php
+++ views/frontend/template.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--Bootstrap CDN-->
+        <link rel="stylesheet" href="public/css/bootstrap.min.css" >
         <!--CSS-->
         <link rel="stylesheet" type="text/css" href="public/css/style.css">
-        <!--Bootstrap CDN-->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title><?= $title ?></title>
     </head>
         
