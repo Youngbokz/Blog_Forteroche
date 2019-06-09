0813697aef9f06aabed724a9854cf5cb15339c1b
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-09 16:34:05 +0200

    set subscribe's conditions in index.php

--- index.php
+++ index.php
@@ -36,82 +36,51 @@ try //
                     if(preg_match('#^[a-zA-Z0-9_]{3,16}$#i', ($username)))
                     {   
                         // Check if login is already used in database
-                        $verifyPseudo = $bdd->prepare('SELECT * FROM member WHERE pseudo = ?');
-                        $verifyPseudo->execute(array($pseudo));
-                        $pseudoExist = $verifyPseudo->rowCount();
-                        if($pseudoExist == 0)
+                        $verifyUsername = verify($username);
+
+                        if($username == 0)
                         {
                             // Check out passwords
                             if(preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$#', ($pass)))
                             {   // Check if passwords are the same
                                 if(($pass) == ($re_pass))
                                 {   
-                                    // Check if email is already used
-                                    $verifyEmail = $bdd->prepare('SELECT * FROM member WHERE email = ?');
-                                    $verifyEmail->execute(array($email));
-                                    $emailExist = $verifyEmail->rowCount();
-                                    if($emailExist == 0)
-                                    {
-                                        // Check out if it s not an email 
-                                        if(!preg_match('#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#i', ($email)))
-                                        {
-                                            $error = '<font color="red">Veuillez entrer un format email valide</font>';
-                                        }//--------------------- END OF if email
-                                        else // if it is we insert data into base
-                                        {   
-                                            // Encrypt password
-                                            $pass_hache = password_hash($pass, PASSWORD_DEFAULT);
-                                            // Insertion 
-                                            $req = $bdd->prepare('INSERT INTO member(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
-                                            $req->execute(array(
-                                            'pseudo' => $pseudo,
-                                            'pass' => $pass_hache,
-                                            'email' => $email)); 
+                                    subscribe($log, $pass); // so we finaly add the member in database
                                             echo '<font color="green">Vous êtes enregistré(e)</font>';   
-
-                                            $req->closeCursor();
-                                        }//--------------------- END OF else email
-                                    }//--------------------- END OF if emailExist
-                                    else
-                                    {
-                                        $error = '<font color="orange">Email déjà utilisé</font>
-                                        <p>Prendre un autre email ou <a href="connexion.php">Connectez-Vous !</a></p>
-                                        <p><a href="index.php">Revenir à la page d\'accueil</a></p>';
-                                    }//--------------------- END OF else emailExist
-                                }//--------------------- END OF if password compare
+                                }//--------------------- END OF compare passwords
                                 else
                                 {
-                                    $error ='<font color="red">Mots de passe non similaires</font>';
-                                }//--------------------- END OF else password compare
-                            }//--------------------- END OF if password
+                                    $errorMessage ='<font color="red">Mots de passe non similaires</font>';
+                                }//--------------------- END OF else compare password
+                            }//--------------------- END OF if password 
                             else
                             {
-                                $error = '<font color="red">Mot de passe 8 caractères minimum avec au moins 1 minuscule, 1 majuscule et 1 chiffre</font>';
-                            }//--------------------- END OF else password
-                        }//--------------------- END OF if pseudoExist
+                                $errorMessage = '<font color="red">Mot de passe 8 caractères minimum avec au moins 1 minuscule, 1 majuscule et 1 chiffre</font>';
+                            }//--------------------- END OF else password post
+                        }//--------------------- END OF if username exist
                         else
                         {
-                            $error= '<font color="purple">Pseudo déjà utilisé !</font> 
+                            $errorMessage= '<font color="purple">Pseudo déjà utilisé !</font> 
                                     <p>Prendre un autre pseudo ou <a href="connexion.php">Connectez-Vous !</a></p>
                                     <p><a href="index.php">Revenir à la page d\'accueil</a></p>';
-                        }//--------------------- END OF else pseudoExist
-                    }//--------------------- END OF if pseudo
+                        }//--------------------- END OF else username exist
+                    }//--------------------- END OF if username post
                     else
                     {
-                        $error = '<font color="red">Votre pseudo doit contenir 3 caractères minimum de lettre miniscules ou/et majuscules ou/et chiffres ou/et _</font>';
-                    }//--------------------- END OF else pseudo
-                    
-                }//--------------------- END OF if !empty()
+                        $errorMessage = '<font color="red">Votre pseudo doit contenir 3 caractères minimum de lettre miniscules ou/et majuscules ou/et chiffres ou/et _</font>';
+                    }//--------------------- END OF else username post
+                }//--------------------- END OF if !empty
                 else
                 {
-                    $error = '<font color="red">Veuillez renseigner tout les champs</font>';
-                }//--------------------- END OF else empty()
+                    $errorMessage = '<font color="red">Veuillez renseigner tout les champs</font>';
+                }//--------------------- END OF else !empty              
             }//--------------------- END OF if submit
             else
             {
-                $error = '<p><a href="index.php">Revenir à la page d\'accueil</a></p>';
-            }
-        }
+                $errorMessage = '<p>L\'envoie des données n\'a pas fonctionné. Veuillez réessayer</p>';
+            }//--------------------- END OF else submit
+        }//--------------------- END OF if subscribe
+
         elseif($_GET['action'] == 'post')
         {
             if(isset($_GET['id']) && $_GET['id'] >0)
