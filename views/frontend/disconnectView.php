62baf879bfe192cb89193af9b5047b6d9d6d80a7
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-12 19:10:42 +0200

    cleaned up disconnect page from no need comments

---
+++ views/frontend/disconnectView.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+
+    // Canceled current session
+    
+    session_destroy();
+
+    // Canceled current cookies login and password
+    
+
+    header('Location: index.php');
+?>
