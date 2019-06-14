169811acce4d731d6ffa8a59e4fb8968751dddce
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-14 21:56:03 +0200

    delete old subscribe form style

--- views/frontend/subscribeView.php
+++ views/frontend/subscribeView.php
@@ -3,27 +3,6 @@
 <?php $title = 'INSCRIPTION | Jean FORTEROCHE'; ?>
 <?php ob_start(); ?>
     <div class="mainSection">
-        <div>
-            <h2>INSCRIPTION</h2>
-        </div>
-        <div>
-            <section>
-                <form action="index.php?action=register" method="post">
-                    <label for="username">Choisir un pseudo</label>
-                    <input type="text" name="username" id="username" placeholder="Nom d'utilisateur"  />
-
-                    <label for="pass">Choisir un mot de passe</label>
-                    <input type="password" name="pass" id="pass" placeholder="Mot de passe"  />
-
-                    <label for="re_pass">Confirmation du mot de passe</label>
-                    <input type="password" name="re_pass" id="re_pass" placeholder="Mot de passe"  />
-
-                    <input type="submit" name="submit" value="Inscription" />
-                </form>
-            </section>
-        </div> 
-    <div>
-    <div class="mainSection">
         <div class="subscribeContainer container ">
             <section class="row">
                 <form class="form-signin" action="index.php?action=register" method="post">
