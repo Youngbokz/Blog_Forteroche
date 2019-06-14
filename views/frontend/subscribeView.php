678b1f93d24a2bd4a601427f1b5a7f16641fc8f4
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-14 21:54:59 +0200

    change subscribe page to look as connexion page style

--- views/frontend/subscribeView.php
+++ views/frontend/subscribeView.php
@@ -23,6 +23,24 @@
             </section>
         </div> 
     <div>
+    <div class="mainSection">
+        <div class="subscribeContainer container ">
+            <section class="row">
+                <form class="form-signin" action="index.php?action=register" method="post">
+                    <h1 class="h3 mb-3 font-weight-normal">INSCRIPTION</h1>
+                    <label for="username" >Choisir nom d'utilisateur</label>
+                    <input type="text" id="username" name="username" class="form-control" placeholder="Nom d'utilisateur" required="" autofocus="">
+                    <label for="pass" >Choisir mot de passe</label>
+                    <input type="password" id="pass" name="pass" class="form-control" placeholder="Mot de passe" required="">
+                    <label for="re_pass" >Confirmer mot de passe</label>
+                    <input type="password" id="re_pass" name="re_pass" class="form-control" placeholder="Mot de passe" required="">
+                    
+                    <input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="S'inscrire">
+                    <p class="mt-5 mb-3 text-muted">© 2018-2019</p>
+                </form>
+            </section>
+        </div> 
+    </div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?> 
