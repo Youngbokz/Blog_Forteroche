2fc6334149f530e403c4ebe5dc482403cf1a7aa4
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-06-17 00:53:49 +0200

    add id to li of side navbar admin page

--- views/frontend/adminView.php
+++ views/frontend/adminView.php
@@ -15,10 +15,10 @@ if(isset($_SESSION['login']))
         <a class="navbar-brand" href="#">Tableau de Bord</a>
         </div>
         <ul class="nav navbar-nav">
-        <li class="active"><a href="#"><span><i class="fas fa-user"></i></span><span> Utilisateur(s)</span></a></li>
-        <li><a href="#"><span><i class="fas fa-scroll"></i></span><span> Article(s) posté(s)</span></a></li>
-        <li><a href="#"><span><i class="fas fa-feather-alt"></i></span><span> Créer</span></a></li>
-        <li><a href="#"><span><i class="fas fa-comments"></i></span><span> Commentaire(s)</span></a></li>
+        <li id="usersList" class="active"><a href="#"><span><i class="fas fa-user"></i></span><span> Utilisateur(s)</span></a></li>
+        <li id="postsList"><a href="#"><span><i class="fas fa-scroll"></i></span><span> Article(s) posté(s)</span></a></li>
+        <li id="creatNewPost"><a href="#"><span><i class="fas fa-feather-alt"></i></span><span> Créer</span></a></li>
+        <li id="commentsList"><a href="#"><span><i class="fas fa-comments"></i></span><span> Commentaire(s)</span></a></li>
         </ul>
     </div>
     </nav>
